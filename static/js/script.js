4a9c8dcd629809978a8ad23cdcb1f3784af6d7f0
joeshisya <ljshisya@gmail.com>
2017-04-14 02:42:49 +0300

    Minor changes to results page - Added toggle capabolity for each years result Minor changes to css file

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,17 @@
-/* off-canvas sidebar toggle */
-$('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-    $('.collapse').toggleClass('in').toggleClass('hidden-xs').toggleClass('visible-xs');
-});
+function toggle(showHideDiv, switchTextDiv)
+{
+	var ele = document.getElementById(showHideDiv);
+	var text = document.getElementById(switchTextDiv);
+
+	if(ele.style.display === "block")
+	{
+	    ele.style.display = "none";
+  	}
+
+	else
+    {
+        ele.style.display = "block";
+
+	}
+
+}
